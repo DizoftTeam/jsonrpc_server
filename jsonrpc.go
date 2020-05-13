44f060e0ca44ee0915b872bddbd309cc7b0b6ec9
WiRight <ruslan.mahin1@gmail.com>
2020-05-13 12:51:23 +0300

    Use reflect.Slice instead hard string

--- jsonrpc.go
+++ jsonrpc.go
@@ -1,20 +1,20 @@
 package jsonrpc_server
 
 import (
-    "github.com/DizoftTeam/jsonrpc_server/utils"
+	"github.com/DizoftTeam/jsonrpc_server/utils"
 
-    "github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/mapstructure"
 
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "reflect"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
 )
 
 const (
-    rpcVersion = "2.0" // Supported protocol version
+	rpcVersion = "2.0" // Supported protocol version
 )
 
 // Array of methods
@@ -22,16 +22,16 @@ var methods = map[string]Method{}
 
 // RPCRequest RPC struct
 type RPCRequest struct {
-    Version string      `mstruct:"jsonrpc"` // Protocol version
-    Method  string      `mstruct:"method"`  // Method name
-    Params  interface{} `mstruct:"params"`  // Method params
-    ID      int         `mstruct:"id"`      // Request id
+	Version string      `mstruct:"jsonrpc"` // Protocol version
+	Method  string      `mstruct:"method"`  // Method name
+	Params  interface{} `mstruct:"params"`  // Method params
+	ID      int         `mstruct:"id"`      // Request id
 }
 
 // RPCError Error struct
 type RPCError struct {
-    Code    int    // Error code
-    Message string // Error message
+	Code    int    // Error code
+	Message string // Error message
 }
 
 // Method Alias on func
@@ -39,130 +39,129 @@ type Method func(params interface{}) (interface{}, *RPCError)
 
 // RPCMethod Interface for struct style method
 type RPCMethod interface {
-    Handler(params interface{}) (interface{}, *RPCError)
+	Handler(params interface{}) (interface{}, *RPCError)
 }
 
 // ----------- STRUCT METHODS -----------
 
 // Register Add method based on struct style
 func Register(name string, handler RPCMethod) {
-    RegisterFunc(name, handler.Handler)
+	RegisterFunc(name, handler.Handler)
 }
 
 // RegisterFunc Add method based on lambda func
 func RegisterFunc(name string, method Method) {
-    methods[name] = method
+	methods[name] = method
 
-    log.Printf("Register method: %v\n", name)
+	log.Printf("Register method: %v\n", name)
 }
 
 // --------------- PUBLIC ---------------
 
 // Handler Main point function
 func Handler(w http.ResponseWriter, r *http.Request) {
-    defer r.Body.Close()
-    w.Header().Add("Content-Type", "application/json")
+	defer r.Body.Close()
+	w.Header().Add("Content-Type", "application/json")
 
-    var request interface{}
-    var response interface{}
+	var request interface{}
+	var response interface{}
 
-    if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-        // TODO:
-        _, _ = fmt.Fprintf(w, "Common error")
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		_, _ = fmt.Fprintf(w, `{"jsonrpc": "2.0", "error": {"code": -42700, "message": "Common Error"}}`)
 
-        return
-    }
+		return
+	}
 
-    reqType := reflect.ValueOf(request).Kind()
+	reqType := reflect.ValueOf(request).Kind()
 
-    if reqType.String() == "slice" {
-        var responses []string
+	if reqType == reflect.Slice {
+		var responses []string
 
-        for _, item := range request.([]interface{}) {
-            responses = append(responses, processRequest(item.(map[string]interface{})))
-        }
+		for _, item := range request.([]interface{}) {
+			responses = append(responses, processRequest(item.(map[string]interface{})))
+		}
 
-        // TODO: maybe it can make more beautiful
-        response = "[" + strings.Join(responses, ",") + "]"
-    } else {
-        response = processRequest(request.(map[string]interface{}))
-    }
+		// TODO: maybe it can make more beautiful
+		response = "[" + strings.Join(responses, ",") + "]"
+	} else {
+		response = processRequest(request.(map[string]interface{}))
+	}
 
-    _, _ = fmt.Fprint(w, response)
+	_, _ = fmt.Fprint(w, response)
 }
 
 // EmptyRequestError Wrong data or Empty request
 // Helper function for most cases
 func EmptyRequestError() (interface{}, *RPCError) {
-    return nil, &RPCError{
-        Code:    -20,
-        Message: "Wrong data or Empty request",
-    }
+	return nil, &RPCError{
+		Code:    -20,
+		Message: "Wrong data or Empty request",
+	}
 }
 
 // --------------- PRIVATE ---------------
 
 func processRequest(request utils.Object) string {
-    req := RPCRequest{}
+	req := RPCRequest{}
 
-    decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
-        Result:  &req,
-        TagName: "mstruct",
-    })
+	decoder, _ := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		Result:  &req,
+		TagName: "mstruct",
+	})
 
-    // Check wrong JSON data
-    if err := decoder.Decode(request); err != nil {
-        return performError(req, -32700, "Parse error")
-    }
+	// Check wrong JSON data
+	if err := decoder.Decode(request); err != nil {
+		return performError(req, -32700, "Parse error")
+	}
 
-    // Check RPC format
-    if req.Version != rpcVersion {
-        return performError(req, -32600, "Invalid Request")
-    }
+	// Check RPC format
+	if req.Version != rpcVersion {
+		return performError(req, -32600, "Invalid Request")
+	}
 
-    // If method not found
-    if _, ok := methods[req.Method]; !ok {
-        return performError(req, -32601, "Method not found")
-    }
+	// If method not found
+	if _, ok := methods[req.Method]; !ok {
+		return performError(req, -32601, "Method not found")
+	}
 
-    // Run method
-    method := methods[req.Method]
-    result, rpcError := method(req.Params)
+	// Run method
+	method := methods[req.Method]
+	result, rpcError := method(req.Params)
 
-    // Check is not notify type
-    if req.ID != 0 {
-        if rpcError != nil {
-            return performError(req, rpcError.Code, rpcError.Message)
-        }
+	// Check is not notify type
+	if req.ID != 0 {
+		if rpcError != nil {
+			return performError(req, rpcError.Code, rpcError.Message)
+		}
 
-        return performSuccess(req, result)
-    }
+		return performSuccess(req, result)
+	}
 
-    return ""
+	return ""
 }
 
 // performError Format success response
 func performSuccess(rpc RPCRequest, data interface{}) string {
-    return performResponse(rpc, "result", data)
+	return performResponse(rpc, "result", data)
 }
 
 // performError Format error response
 func performError(rpc RPCRequest, code int, message string) string {
-    return performResponse(rpc, "error", utils.Object{
-        "code":    code,
-        "message": message,
-    })
+	return performResponse(rpc, "error", utils.Object{
+		"code":    code,
+		"message": message,
+	})
 }
 
 // performResponse Create response
 func performResponse(rpc RPCRequest, key string, value interface{}) string {
-    _struct := utils.Object{
-        "jsonrpc": "2.0",
-        "id":      rpc.ID,
-        key:       value,
-    }
+	_struct := utils.Object{
+		"jsonrpc": "2.0",
+		"id":      rpc.ID,
+		key:       value,
+	}
 
-    result, _ := json.Marshal(_struct)
+	result, _ := json.Marshal(_struct)
 
-    return string(result)
+	return string(result)
 }
